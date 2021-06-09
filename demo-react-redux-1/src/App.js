2072a51519818f4a19dd04a054386106b3a2e062
xu.hongrui <xu.hongrui@uneed.com>
2021-06-09 15:00:24 +0800

    feat:增加react-redux

--- demo-react-redux-1/src/App.js
+++ demo-react-redux-1/src/App.js
@@ -1,31 +1,70 @@
-import React, { Component } from 'react'
+
+// 使用react 和 redux
+// import React, { Component } from 'react'
+
+// class App extends Component {
+//   add1(){
+//     this.props.onAdd1()
+//   }
+//   add2(){
+//     this.props.onAdd2()
+//   }
+//   addIf(){
+//     this.props.onAddIf()
+//   }
+//   add1After2sec(){
+//     this.props.add1After2sec()
+//   }
+//   render(){
+//     return (
+//       <div>
+//         你点击了<span id="value">{this.props.value}</span>次
+//         <div>
+//           <button id="add1" onClick={this.add1.bind(this)}>+1</button>
+//           <button id="add2"  onClick={this.add2.bind(this)}>+2</button>
+//           <button id="add1IfOdd"  onClick={this.addIf.bind(this)}>如果是单数就+1</button>
+//           <button id="add1After2sec" onClick={this.add1After2sec.bind(this)}>两秒钟后+1</button>
+//         </div>
+//       </div>
+//     )
+//   }
+// }
+
+// export default App;
+
+
+
+// 使用react 和 react-redux
+
+import React, { Component } from 'react';
+import { connect } from "react-redux";
+
 
 class App extends Component {
-  add1(){
-    this.props.onAdd1()
-  }
-  add2(){
-    this.props.onAdd2()
-  }
-  addIf(){
-    this.props.onAddIf()
-  }
-  add1After2sec(){
-    this.props.add1After2sec()
-  }
-  render(){
+  render() {
     return (
       <div>
-        你点击了<span id="value">{this.props.value}</span>次
+        你点击了 <span id="value">{this.props.n}</span> 次
         <div>
-          <button id="add1" onClick={this.add1.bind(this)}>+1</button>
-          <button id="add2"  onClick={this.add2.bind(this)}>+2</button>
-          <button id="add1IfOdd"  onClick={this.addIf.bind(this)}>如果是单数就+1</button>
-          <button id="add1After2sec" onClick={this.add1After2sec.bind(this)}>两秒钟后+1</button>
+          <button id="add1" onClick={()=> this.props.add1()}>+1</button>
+          <button id="add2">+2</button>
+          <button id="add1IfOdd">如果是单数就+1</button>
+          <button id="add1After2Sec">两秒钟后+1</button>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default App;
+function mapStateToProps(state){
+  return {
+    n: state.n
+  }
+}
+function mapDispatchToProps(dispatch) {
+  return {
+    add1: ()=> dispatch({type:'add', payload: 1})
+  }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(App);
--- demo-react-redux-1/src/index.js
+++ demo-react-redux-1/src/index.js
@@ -1,49 +1,83 @@
+
+// 使用react 和 redux  
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+// import { createStore } from 'redux'
+
+// const stateChanger = (state, action) => {
+//   if (typeof state === 'undefined') {
+//     return 0
+//   }
+//   switch (action.type) {
+//     case 'add':
+//       return state + action.payload
+ 
+//     default:
+//       return state
+//   }
+// }
+
+// const store = createStore(stateChanger)
+// store.subscribe(render)
+// function render(){
+//   ReactDOM.render(
+//     <React.StrictMode>
+//       <App  value={store.getState()}  
+//       onAdd1={() => store.dispatch({ type: 'add', payload: 1 })} 
+//       onAdd2={() => store.dispatch({ type: 'add', payload: 2 })}
+//       onAddIf={() => {
+//         let oldstate = store.getState()
+//         if(oldstate % 2 == 1){
+//           store.dispatch({ type: 'add', payload: 1 })
+//         }
+//       }}
+//       add1After2sec={() => {
+//         setTimeout(() => {
+//           store.dispatch({ type: 'add', payload: 1 })
+//         }, 2000)
+//       }}/>
+//     </React.StrictMode>,
+//     document.getElementById('root')
+//   );
+// }
+// render()
+// reportWebVitals();
+
+
+// 使用react 和 react-redux 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { createStore } from 'redux'
+// import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux'
+import { Provider } from "react-redux";
 
-const stateChanger = (state, action) => {
-  if (typeof state === 'undefined') {
-    return 0
-  }
-  switch (action.type) {
-    case 'add':
-      return state + action.payload
- 
-    default:
+const reducer = (state, action)=>{
+  if(state === undefined){
+    return {n: 0}
+  }else{
+    if(action.type === 'add'){
+      var newState = {n: state.n + action.payload}
+      return newState
+    }else{
       return state
+    }
   }
 }
+const store = createStore(reducer)
 
-const store = createStore(stateChanger)
-store.subscribe(render)
-function render(){
-  ReactDOM.render(
-    <React.StrictMode>
-      <App  value={store.getState()}  
-      onAdd1={() => store.dispatch({ type: 'add', payload: 1 })} 
-      onAdd2={() => store.dispatch({ type: 'add', payload: 2 })}
-      onAddIf={() => {
-        let oldstate = store.getState()
-        if(oldstate % 2 == 1){
-          store.dispatch({ type: 'add', payload: 1 })
-        }
-      }}
-      add1After2sec={() => {
-        setTimeout(() => {
-          store.dispatch({ type: 'add', payload: 1 })
-        }, 2000)
-      }}/>
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-}
-render()
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  document.getElementById('root')
+);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+// serviceWorker.unregister();
